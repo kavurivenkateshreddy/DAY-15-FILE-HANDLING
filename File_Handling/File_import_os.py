0a2efe8ae30b870e1c0b0a6c66cce7130c9cdd49
Venkatesh Reddy Kavuri <104256379+kavurivenkateshreddy@users.noreply.github.com>
2022-07-11 10:14:57 +0530

    DAY-15 FILE HANDLING

---
+++ File_Handling/File_import_os.py
@@ -0,0 +1,10 @@
+# import os
+# print(os.getcwd())
+
+import os
+# os.mkdir('4pm batch')
+# os.rmdir('4pm batch')
+# os.remove('josh940Am.txt')
+# print(os.path.exists('Josh2pm.txt'))
+# print(os.path.exists('Josh.txt'))
+# print(os.path.exists('2pm BaTch'))
